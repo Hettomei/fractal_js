e36a98dc358d67f7413fbe952de5da6d9be4b43e
Timothée Gauthier <timothee.gauthier@consertotech.pro>
2019-06-28 16:04:19 +0200

    code formatting

--- lib_js/main.js
+++ lib_js/main.js
@@ -1,14 +1,16 @@
 window.onload = function(){
   var board = new Board('mon_canvas');
+  board.clean_background();
   var lines = []
 
   var origin = new Point(10, window.innerHeight / 2 + 200);
   var angle = 0 * Math.PI;
   var length = window.innerWidth - 60 ;
   // From 1 to 10. Higger may kill browser
-  const precision = 8;
+  const precision = 7;
 
   lines.push(new Line(origin, length, angle, board.context));
+  let future_lines = [];
 
   for(var i = 0; i < precision; i++){
     future_lines = [];
@@ -18,12 +20,9 @@ window.onload = function(){
       future_lines.push(line.split_3());
       future_lines.push(line.split_4());
     });
-    lines=future_lines;
+    lines = future_lines;
+    // lines.forEach((entry) => entry.draw());
   }
 
-  board.clean_background();
-  lines.forEach(function(entry) {
-    entry.draw();
-  });
-
+  lines.forEach((entry) => entry.draw());
 };
